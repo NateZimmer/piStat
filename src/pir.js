efb83f485795bf595bf3d9e44b97be2e327fbe7a
Nate <nate.m.zimmerman@gmail.com>
2019-11-16 06:50:49 -0600

    .

--- src/pir.js
+++ src/pir.js
@@ -12,7 +12,7 @@ var changeTimeout = 50;
 
 async function setup(){
 	var done = await gpiop.setup(state.getProp('led_pin'), gpio.DIR_OUT);
-	done = await gpiop.write(state.getProp('led_pin'), false)
+	done = await gpiop.write(state.getProp('led_pin'), true)
 	gpio.setup(state.getProp('sensePin'), gpio.DIR_IN, gpio.EDGE_BOTH);
 
 	gpio.on('change', function(channel, value) {
